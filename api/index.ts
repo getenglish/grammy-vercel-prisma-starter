cb883f7cbb88ac1e5aa53ccce447aeadeb65587e
JustTrott <greenvisp@gmail.com>
2025-03-20 00:56:18 +0100

    refactor: fix referenceError of convertKeysToCamelCase

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,11 @@
 import process from 'node:process'
 import { createBot } from '#root/bot/index.js'
-import { createConfig } from '#root/config.js'
+import { convertKeysToCamelCase, createConfig } from '#root/config.js'
 import { createLogger } from '#root/logger.js'
 import { prisma } from '#root/prisma/index.js'
 import { createServer } from '#root/server/index.js'
 import { handle } from '@hono/node-server/vercel'
 
-// @ts-expect-error create config from environment variables
 const config = createConfig(convertKeysToCamelCase(process.env))
 const logger = createLogger(config)
 
--- src/config.ts
+++ src/config.ts
@@ -46,36 +46,36 @@ export type Config = v.InferOutput<typeof configSchema>
 export type PollingConfig = v.InferOutput<typeof configSchema['options'][0]>
 export type WebhookConfig = v.InferOutput<typeof configSchema['options'][1]>
 
-export function createConfig(input: v.InferInput<typeof configSchema>) {
+export function createConfig(input: Record<string, unknown>): Config {
   return v.parse(configSchema, input)
 }
 
 export const config = createConfigFromEnvironment()
 
-function createConfigFromEnvironment() {
-  type CamelCase<S extends string> = S extends `${infer P1}_${infer P2}${infer P3}`
-    ? `${Lowercase<P1>}${Uppercase<P2>}${CamelCase<P3>}`
-    : Lowercase<S>
+type CamelCase<S extends string> = S extends `${infer P1}_${infer P2}${infer P3}`
+  ? `${Lowercase<P1>}${Uppercase<P2>}${CamelCase<P3>}`
+  : Lowercase<S>
 
-  type KeysToCamelCase<T> = {
-    [K in keyof T as CamelCase<string & K>]: T[K] extends object ? KeysToCamelCase<T[K]> : T[K]
-  }
+type KeysToCamelCase<T> = {
+  [K in keyof T as CamelCase<string & K>]: T[K] extends object ? KeysToCamelCase<T[K]> : T[K]
+}
 
-  function toCamelCase(str: string): string {
-    return str.toLowerCase().replace(/_([a-z])/g, (_match, p1) => p1.toUpperCase())
-  }
+function toCamelCase(str: string): string {
+  return str.toLowerCase().replace(/_([a-z])/g, (_match, p1) => p1.toUpperCase())
+}
 
-  function convertKeysToCamelCase<T>(obj: T): KeysToCamelCase<T> {
-    const result: any = {}
-    for (const key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        const camelCaseKey = toCamelCase(key)
-        result[camelCaseKey] = obj[key]
-      }
+export function convertKeysToCamelCase<T>(obj: T): KeysToCamelCase<T> {
+  const result: any = {}
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const camelCaseKey = toCamelCase(key)
+      result[camelCaseKey] = obj[key]
     }
-    return result
   }
+  return result
+}
 
+function createConfigFromEnvironment() {
   try {
     process.loadEnvFile()
   }
@@ -84,9 +84,7 @@ function createConfigFromEnvironment() {
   }
 
   try {
-    // @ts-expect-error create config from environment variables
     const config = createConfig(convertKeysToCamelCase(process.env))
-
     return config
   }
   catch (error) {
